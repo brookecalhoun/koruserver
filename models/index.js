40035f7a96af1ea580ceac5ec197e8f0094eb01d
brookecalhoun <86681030+brookecalhoun@users.noreply.github.com>
2021-09-24 13:36:17 -0700

    commit

--- models/index.js
+++ models/index.js
@@ -7,10 +7,10 @@ const configOptions = {
   useUnifiedTopology: true,
 
 };
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
 mongoose.connect(connectionString, configOptions)
   .then(() => console.log('MongoDB successfully connected...'))
   .catch((err) => console.log(`MongoDB connection error: ${err}`));
