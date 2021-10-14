00674303897953081c9f89c2f443dc8c817a40ff
brookecalhoun <86681030+brookecalhoun@users.noreply.github.com>
2021-10-14 16:26:19 -0700

    commit

--- middleware/checkJwt.js
+++ middleware/checkJwt.js
@@ -8,7 +8,7 @@ const checkJwt = jwt({
         jwksRequestsPerMinute: 5,
         jwksUri: 'https://dev-96jkcqer.us.auth0.com/.well-known/jwks.json'
     }),
-    audience: 'http://localhost:4000',
+    audience: 'https://koru-server.herokuapp.com/api/journal',
     issuer: 'https://dev-96jkcqer.us.auth0.com/',
     algorithms: ['RS256']
 })
